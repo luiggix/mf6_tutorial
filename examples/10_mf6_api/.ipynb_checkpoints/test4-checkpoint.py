d114928affbfaa07570be38165bcbfd520971dd3
luiggix <luiggix@gmail.com>
2025-07-28 18:41:37 -0600

    agregué test4 y test5 con ejemplos de transporte usando la API

--- examples/10_mf6_api/.ipynb_checkpoints/test4-checkpoint.py
+++ examples/10_mf6_api/.ipynb_checkpoints/test4-checkpoint.py
@@ -1,45 +1,16 @@
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
-import os, sys
 import flopy
-from modflowapi import ModflowApi
+from modflowapi import ModflowApi # La API
 import xmf6
 
-def build_mat(mf6):
-    """
-    Construye la matriz del sistema.
-
-    Parameters
-    ----------
-    mf6: ModflowApi
-        Objeto para accedar a toda la funcionalidad de la API.
-    """
-    # Obtiene el número de renglones y columnas del sistema
-    NCOL = mf6.get_value(mf6.get_var_address("NCOL", 'SLN_1'))
-    NROW = mf6.get_value(mf6.get_var_address("NROW", 'SLN_1'))
-
-    # Obtiene los coeficientes de la matriz en formato CRS (Compressed Row Storage)
-    # A: Coeficientes, JA: índices de la columna, IA: índice de inicio del renglón en JA.
-    A = mf6.get_value(mf6.get_var_address("AMAT", 'SLN_1'))
-    IA = mf6.get_value(mf6.get_var_address("IA", 'SLN_1'))
-    JA = mf6.get_value(mf6.get_var_address("JA", 'SLN_1'))
-
-    # Arreglo para almacenar la matriz en formato completo.
-    Atest = np.zeros((NROW[0], NCOL[0]))
-    idx = 0
-    i = 0
-    istart = IA[0] # Inicio del renglón en IA
-    for iend in IA[1:]: # Recorremos desde el inicio de cada renglón
-        for j in range(istart, iend): # Recorremos todos los elementos del renglón
-            Atest[idx, JA[j-1]-1] = A[i] # Agregamos el coeficiente en la matriz completa
-            i += 1
-        istart = iend
-        idx += 1
-    return Atest, A, IA, JA # Regresamos la matriz densa y en el format CRS
-
-# --- Preparación de la simulación ---
-
-# --- Componentes ---
+phys = dict(
+    specific_discharge = 0.1,  # Specific discharge ($cm s^{-1}$)
+    hydraulic_conductivity = 0.01,  # Hydraulic conductivity ($cm s^{-1}$)
+    source_concentration = 1.0,  # Source concentration (unitless)
+)
+xmf6.nice_print(phys, "Parámetros físicos")
 
 # Parámetros de la simulación (flopy.mf6.MFSimulation)
 init = {
@@ -51,9 +22,9 @@ init = {
 
 # Parámetros para el tiempo (flopy.mf6.ModflowTdis)
 tdis = {
-    'units': "DAYS",
-    'nper' : 3,
-    'perioddata': [(1.0, 5, 1.0), (2.0, 3, 1.2), (3.0, 4, 1.1)] 
+    'units': "seconds",
+    'nper' : 1,
+    'perioddata': [(120.0, 1, 1.0)]
 }
 
 # Parámetros para la solución numérica (flopy.mf6.ModflowIms)
@@ -62,33 +33,20 @@ ims = {}
 # Parámetros para el modelo de flujo (flopy.mf6.ModflowGwf)
 gwf = { 
     'modelname': init["sim_name"],
-    'model_nam_file': f"{init["sim_name"]}.nam",
+#    'model_nam_file': f"{init["sim_name"]}.nam",
     'save_flows': True
 }
 
-# --- Paquetes del modelo de flujo ---
-lx = 25
-ly = 25
-nrow = 20 #int(input("nrow = "))
-ncol = 20 #int(input("ncol = "))
-#if nrow > 10 or nrow < 0 or ncol > 10 or ncol < 0:
-#    print("Este ejemplo está limitado a:")
-#    print(" 0 < nrow =< 10")
-#    print(" 0 < ncol =< 10")
-#    sys.exit()
-delr = lx / ncol
-delc = ly / nrow 
-
 # Parámetros para la discretización espacial (flopy.mf6.ModflowGwfdis)
 dis = {
-    'length_units': "meters",
-    'nlay': 5, 
-    'nrow': nrow, 
-    'ncol': ncol,
-    'delr': delr, 
-    'delc': delc, 
-    'top' : 0.0, 
-    'botm': [-2.0, -4.0, -6.0, -8.0, -10.0] 
+    'length_units' : "centimeters",
+    'nlay': 1, 
+    'nrow': 1, 
+    'ncol': 120,
+    'delr': 0.1, 
+    'delc': 0.1, 
+    'top' : 1.0, 
+    'botm': 0.0 
 }
 
 # Parámetros para las condiciones iniciales (flopy.mf6.ModflowGwfic)
@@ -97,35 +55,33 @@ ic = {
 }
 
 # Parámetros para las condiciones de frontera (flopy.mf6.ModflowGwfchd)
-chd_data = []
-for row in range(dis['nrow']):
-    chd_data.append([(0, row, 0), 10.0])       # Condición en la pared izquierda
-    chd_data.append([(0, row, dis['ncol'] - 1), 5.0]) # Condición en la pared derecha
-
 chd = {
-    'stress_period_data': chd_data,     
+    'stress_period_data': [[(0, 0, dis['ncol'] - 1), 1.0]],     
 }
 
 # Parámetros para las propiedades de flujo (flopy.mf6.ModflowGwfnpf)
 npf = {
     'save_specific_discharge': True,
-    'save_saturation' : True,
-    'icelltype' : 0,
-    'k' : 0.01,
+    'save_saturation': True,
+    'icelltype':  0,
+    'k': 0.01  # Hydraulic conductivity ($cm s^{-1}$) 
 }
 
-
-# === Pozo en el centro con caudal variable ===
-well_data = {
-    0: [[(1, 5, 5), -0.5]],
-    1: [[(1, 5, 5), -0.025]],
-    2: [[(1, 5, 5), -0.015]],
-}
+phys = dict(
+    specific_discharge = 0.1,  # Specific discharge ($cm s^{-1}$)
+    hydraulic_conductivity = 0.01,  # Hydraulic conductivity ($cm s^{-1}$)
+    source_concentration = 1.0,  # Source concentration (unitless)
+)
+# Parámetros para las propiedades de los pozos (flopy.mf6.ModflowGwfwel)
+phys["specific_discharge"] = 0.1 # Specific discharge ($cm s^{-1}$)
+phys["source_concentration"] = 1.0  # Source concentration (unitless)
+q = phys["specific_discharge"] * dis['delc'] * dis['delr'] * dis['top']
 
 well = {
-    'stress_period_data': well_data,
+    'stress_period_data': [[(0, 0, 0), q, phys["source_concentration"],]],
     'pname': "WEL-1",
-    'save_flows': True
+    'auxiliary' : ["CONCENTRATION"],
+#    'save_flows': True
 }
 
 # Parámetros para almacenar y mostrar la salida de la simulación (flopy.mf6.ModflowGwfoc)
@@ -133,84 +89,158 @@ oc = {
     'budget_filerecord': f"{init['sim_name']}.bud",
     'head_filerecord': f"{init['sim_name']}.hds",
     'saverecord': [("HEAD", "ALL"), ("BUDGET", "ALL")],
-    'printrecord': [("HEAD", "ALL")]
+#    'printrecord': [("HEAD", "ALL")]
 }
 
-# Inicialización de la simulación
-o_sim = xmf6.common.init_sim(init = init, tdis = tdis, ims = ims, silent = True)
+# --- Inicialización de la simulación ---
+o_sim = xmf6.common.init_sim(silent = True, init = init, tdis = tdis, ims = ims)
+o_gwf, packages = xmf6.gwf.set_packages(o_sim, silent = True,
+                                        gwf = gwf, 
+                                        dis = dis, ic = ic, chd = chd, npf = npf, oc = oc, well = well)
 
-# Configuración de los paquetes para el modelo de flujo
-o_gwf, package_list = xmf6.gwf.set_packages(o_sim, silent = True,
-                                            gwf = gwf, dis = dis, ic = ic, chd = chd, npf = npf, well = well, oc = oc)
+# --- Escritura de archivos ---
+o_sim.write_simulation(silent = True)
 
-# Escritura de los archivos de entrada
-o_sim.write_simulation(silent = False)
+# --- Ejecución de la simulación ---
+#o_sim.run_simulation(silent = True)
 
-# Ejecución de la simulación
-o_sim.run_simulation()
+###########
+
+OS = sys.platform
+if OS == "win32":
+    mf6_lib = "libmf6.dll"
+elif OS == "darwin":
+    mf6_lib = "libmf6.dylib"
+else:
+    mf6_lib = "libmf6.so"
+
+# Rutas a la biblioteca compartida y al archivo de configuración
+mf6_lib_path = os.path.abspath(os.path.join("..", "..", "mf6", "windows", mf6_lib))
+mf6_config_file = os.path.join(o_sim.sim_path, 'mfsim.nam')
+print("Shared library:", mf6_lib_path)
+print("Config file:", mf6_config_file)
+
+# Objeto para acceder a toda la funcionalidad de la API
+mf6 = ModflowApi(mf6_lib_path, working_directory=o_sim.sim_path)
+
+# Inicialización del modelo
+mf6.initialize(mf6_config_file)
+
+# Para la solución obtenida con np.linalg.solve()
+SOL = np.zeros(dis['ncol'])
+
+# Obtenemos el tiempo actual y el tiempo final de la simulación
+current_time = mf6.get_current_time()
+end_time = mf6.get_end_time()
+
+# Máximo número de iteraciones para el algorimo de solución numérica
+max_iter = mf6.get_value(mf6.get_var_address("MXITER", "SLN_1"))
+
+linea = 50*chr(0x2015)
+print(linea)
+print("Iniciando la simulación")
+print(linea)
+
+# Ciclo sobre tiempo
+while current_time < end_time:
+    # Obtenemos el paso de tiempo
+    dt = mf6.get_time_step()
+    print("dt:", dt, ", t:", current_time, ", end_t:", end_time, ", max_iter:", max_iter)
+
+    # Preparar el objeto de la API para obtener la solución y
+    # con el paso de tiempo
+    mf6.prepare_time_step(dt)
+    mf6.prepare_solve()
+    
+    # Ciclo del algoritmo numérico de solución
+    kiter = 0
+    while kiter < max_iter:
+        print("\nkiter :", kiter)
+        
+        # Construye el sistema del problema y lo resuelve
+        has_converged = mf6.solve(1)
+        
+        if has_converged:
+            print(f" ---> ¿Convergencia obtenida? : {has_converged}")
+            break
+        else:
+            print(f" ---> ¿Convergencia obtenida? : {has_converged}")
+            
+        kiter += 1
+
+    # En este momento podemos construir la matriz del sistema
+    A, _, _, _ = xmf6.api.build_mat(mf6)
+    RHS = mf6.get_value(mf6.get_var_address("RHS", 'SLN_1'))
+    print("\nA:\n", A)
+    print("\nRHS:\n", RHS)
+
+    # Calculamos la solución con np.linalg.solve() para comparar
+    SOL[:] = np.linalg.solve(A, RHS)
+    print("\nSOL:\n", SOL)
+        
+    # Finalizamos la solución del paso de tiempo actual
+    mf6.finalize_solve()
+
+    # Finalizamos el paso de tiempo actual. 
+    mf6.finalize_time_step()
+
+    # Avanzamos en el tiempo
+    current_time = mf6.get_current_time()
+
+    if not has_converged:
+        print("model did not converge")
+        break
+
+# Almacenamos la solución obtenida por MF6 (ojo: necesitamos hacer una copia del arreglo)
+SOL_MF6 = np.copy(mf6.get_value_ptr(mf6.get_var_address("X", 'FLOW')))
+
+# Finalizamos la simulación completa
+try:
+    mf6.finalize()
+    success = True
+except:
+    raise RuntimeError
+
+print(linea)
+print("SOl (MF6):", SOL_MF6)
+print(linea)
+print("Finalizando la simulación")
+print(linea)
+
+###########
+
 
 # --- Recuperamos los resultados de la simulación ---
-head, harray = xmf6.gwf.get_head(o_gwf, binary=True)
-qx, qy, qz, n_q = xmf6.gwf.get_specific_discharge(o_gwf, text="DATA-SPDIS")
+head = xmf6.gwf.get_head(o_gwf)
 
-print(harray.shape)
-# Obtener tiempos disponibles
-times = head.get_times()
-print("Tiempos disponibles:")
-[print(t) for t in times]
-
-# Obtener número de periodos y pasos
-nper = len(times)
-print(nper)
-
-# --- Parámetros para las gráficas ---
-grid = o_gwf.modelgrid
-x, y, z = grid.xyzcellcenters
-hvmin = np.nanmin(harray)
-hvmax = np.nanmax(harray)
-xticks = x[0]
-yticks = y[:,0]
-zticks = z[:,0,0]
-xlabels = [f'{x:1.1f}' for x in xticks]
-ylabels = [f'{y:1.1f}' for y in yticks]
-zlabels = [f'{z:1.1f}' for z in zticks]
-
-ilay = int(input("Capa [1,2,3,4,5] = ")) - 1
+x, y, z = o_gwf.modelgrid.xyzcellcenters
+row_length = o_gwf.modelgrid.extent[1]
 
 # --- Definición de la figura ---
-fig, (ax1, ax2) = plt.subplots(1, 2, width_ratios=[1,1])
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize =(8,4), height_ratios=[2,0.1])
 
 # --- Gráfica 1. ---
-hview = flopy.plot.PlotMapView(model = o_gwf, ax = ax1)
-hview.plot_grid(linewidths = 0.5, alpha = 0.5)
-h_ac = hview.plot_array(harray[ilay,:,:], cmap = "YlGnBu", vmin = hvmin, vmax = hvmax, alpha = 0.75)
-h_cb = plt.colorbar(h_ac, ax = ax1, label = "", cax = xmf6.vis.cax(ax1, h_ac))
-h_cb.ax.tick_params(labelsize=6)
+iskip = 3
+# Carga hidráulica obtenida con mf6.solve() almacenada en memoria.
+ax1.scatter(x[0][::iskip], SOL_MF6[::iskip], marker="o", s = 40, fc = 'darkorange', ec = 'k', alpha=0.75, label="Mf6", zorder=5)
 
-# Visualización de los vectores de la descarga específica.
-ax1.quiver(x, y, qx[0], qy[0], scale = 0.05, 
-           color = 'k', linewidth = 0.95, pivot = 'middle')
+# Carga hidráulica obtenida con np.linalg.solve() almacenada en memoria.
+ax1.scatter(x[0][::iskip], SOL[::iskip], marker="^", s = 10, c='blue', alpha = 0.75, label='np.linalg.solve()', zorder=5)
 
-ax1.set_title("$h$ (Mf6)", fontsize=10)
-ax1.set_ylabel("$y$ (m)", fontsize = 8)
-ax2.set_xlabel("$x$ (m)", fontsize = 8)
-ax1.set_xticks(ticks = [], labels = [], fontsize=6, rotation=90)
-ax1.set_yticks(ticks = yticks, labels = yticks, fontsize=6)
-ax1.set_xticks(ticks = xticks, labels = xticks, fontsize=6, rotation=90)
-ax1.set_aspect('equal')
+ax1.plot(x[0], SOL, c="k", zorder=0)
+
+ax1.set_xlim(0, 12)
+ax1.set_xticks(ticks=np.linspace(0, row_length,13))
+ax1.set_xlabel("Distance (cm)")
+ax1.set_ylabel("Head")
+ax1.grid()
+ax1.legend()
 
 # --- Gráfica 2. ---
-xz6 = flopy.plot.PlotCrossSection(model=o_gwf, ax = ax2, line={"row": 3})
-xz6.plot_grid(linewidths = 0.5, alpha = 0.5)
-xz6_ac = xz6.plot_array(harray, cmap = "YlGnBu", vmin = hvmin, vmax = hvmax, alpha = 0.75)
-xz6_cb = plt.colorbar(xz6_ac, ax = ax2, label = " ", cax = xmf6.vis.cax(ax2, xz6_ac))
-xz6_cb.ax.tick_params(labelsize=6)
-ax2.set_ylabel("$z$ (m)", fontsize = 8)
-ax2.set_xlabel("$x$ (m)", fontsize = 8)
-ax2.set_xticks(ticks = xticks, labels = xticks, fontsize=6, rotation=90)
-ax2.set_yticks(ticks = zticks, labels = zticks, fontsize=6)
-ax2.set_aspect('equal')
+pmv = flopy.plot.PlotMapView(o_gwf, ax=ax2)
+pmv.plot_grid(colors='dimgray', lw=0.5)
+ax2.set_yticks(ticks=[0, 0.1])#, fontsize=8)
+ax2.set_title("Mesh")
 
-plt.suptitle(f"Capa : {ilay+1}")
 plt.tight_layout()
 plt.show()
